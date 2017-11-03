2cc5e0202aa0d90f40aa7a11f9162d0f03e990c8
Mohamed Nassar <mohamedanassar@gmail.com>
2017-11-03 11:07:53 +0100

    Enhanced test case for sorting part 1

--- TakeAwayRestaurantsTests/TakeAwayRestaurantsTests.swift
+++ TakeAwayRestaurantsTests/TakeAwayRestaurantsTests.swift
@@ -51,10 +51,37 @@ class TakeAwayRestaurantsTests: XCTestCase {
     func testRestaurantSort()
     {
         let jsonArray = appController.loadJSONFile(jsonFileName: jsonFileName)
-        let restaurantArray = appController.generateRestaurantArray(fromJSONArray: jsonArray!)
+        guard let restaurantArray = appController.generateRestaurantArray(fromJSONArray: jsonArray!) else {
+            XCTFail("Couldnt parse JSON")
+            return
+        }
+        
         let sortCondition:SortOptions = .BestMatch
         
-        let sortedRestaurantArray = RestaurantSortController.sortRestaurantSubArray(restaurantsSubArray: restaurantArray!, sortOption: sortCondition)
+        let sortedRestaurantArray = RestaurantSortController.sortRestaurantSubArray(restaurantsSubArray: restaurantArray, sortOption: sortCondition)
+        
+        //Now we will check that the sorted array is really sorted.. We will take a value in the middle, and confirm all those after it are lower and all before it are higher.
+        let indexOfSampleRestaurant = sortedRestaurantArray.count/2
+        let sampleRestaurant = sortedRestaurantArray[indexOfSampleRestaurant]
+        
+        for var i in 0..<sortedRestaurantArray.count
+        {
+            if (i == indexOfSampleRestaurant) { //Skip if we are comparing same index
+                i = i+1
+            }
+            
+            let comparisonRestaurant = sortedRestaurantArray[i]
+            //If i is less, then the sort parameter of comparsionRestaurant should be higher than sampleRest
+            if (i < indexOfSampleRestaurant && comparisonRestaurant.bestMatch < sampleRestaurant.bestMatch) {
+                XCTFail("Sorting incorrect. Best match of comparison is less than best match of sample although it shouldn't")
+            }
+            
+            else if (i > indexOfSampleRestaurant && comparisonRestaurant.bestMatch > sampleRestaurant.bestMatch) {
+                XCTFail("Sorting incorrect. Best match of comparison is greater than best match of sample although it shouldn't")
+            }
+            
+            XCTAssertTrue(true, "Sorting Successfull")
+        }
         
     }
     
